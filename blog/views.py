0a51e8cf10844c1273e809090e0c0e345d42cf7e
GAZDA-EDU <gazda.edu@gmail.com>
2021-07-23 22:37:01 +0200

    Dodanie pliku oraz szablonu w HTML

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+#poniższa funkcja post_list pobiera i zwraca wartość uzyskaną dzięki wywołaniu innej funkcji wbudowanej w Django (render), która wyrenderuje (złoży w całość) szablon HTML.
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
